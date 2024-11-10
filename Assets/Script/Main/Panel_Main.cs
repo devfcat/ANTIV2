c5c17b8f89f62bf4c7e76ffdb7e21bffdc3ab6f2
Devfcat <137423690+devfcat@users.noreply.github.com>
2024-11-10 19:25:00 +0900

    [feat] 설정창 기능 스크립트

---
+++ Assets/Script/Main/Panel_Main.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Panel_Main : MonoBehaviour
+{
+    public Slider BgmSlider;
+    public Slider SESlider;
+
+    public void Awake()
+    {
+        Set_UI_Slider();
+    }
+
+    public void Update()
+    {
+        if (BgmSlider.value != SoundManager.Instance.BgmVolume)
+        {
+            Set_UI_Slider();
+        }
+        else if (SESlider.value != SoundManager.Instance.SfxVolume)
+        {
+            Set_UI_Slider();
+        }
+    }
+
+    public void Set_UI_Slider()
+    {
+        BgmSlider.value = SoundManager.Instance.BgmVolume;
+        SESlider.value = SoundManager.Instance.SfxVolume;
+    }
+
+    public void Game_New_Start()
+    {
+        SoundManager.Instance.PlaySFX(SFX.Setting);
+        GameManager.Instance.SetState(eState.Prologue);
+    }
+
+    public void Game_Load_Start()
+    {
+        SoundManager.Instance.PlaySFX(SFX.Setting);
+    }
+
+    public void Game_ReLoad()
+    {
+        SoundManager.Instance.PlaySFX(SFX.Setting);
+        eState now_Scene = GameManager.Instance.m_State;
+        GameManager.Instance.SetState(now_Scene);
+    }
+
+    public void Save_and_Home()
+    {
+        SoundManager.Instance.PlaySFX(SFX.Setting);
+        GameManager.Instance.SetState(eState.Main);
+    }
+
+    public void BTN_close()
+    {
+        GameManager.Instance.Control_Setting();
+    }
+
+    public void Report_Bug()
+    {
+        string mailto = "devfcat@gmail.com";
+        string subject = EscapeURL("[Anti V : 크리스마스 버그 전쟁] 버그 제보 / 문의사항");
+        string body = EscapeURL("내용 : " + "\n");
+        Application.OpenURL("mailto:" + mailto + "?subject=" + subject + "&body=" + body);
+    }
+
+    private string EscapeURL(string url) 
+    {
+        return WWW.EscapeURL(url).Replace("+", "%20"); 
+    }
+
+    public void Quit()
+    {
+        Debug.Log("게임 종료");
+        Application.Quit();
+    }
+}
