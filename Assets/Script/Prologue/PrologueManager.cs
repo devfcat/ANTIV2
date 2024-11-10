78e44abfb35745c357f73d3a6188d8f821cb02d8
Devfcat <137423690+devfcat@users.noreply.github.com>
2024-11-10 19:28:58 +0900

    [feat] 프롤로그 씬 기능 매니저 작업

---
+++ Assets/Script/Prologue/PrologueManager.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+
+public class PrologueManager : MonoBehaviour
+{
+    [Header("패널")]
+    public List<GameObject> Animation_Panels;
+
+    public int panel_Index = 0;
+
+    public GameObject BTN_next;
+    public GameObject BTN_prev;
+
+    private static PrologueManager _instance;
+    public static PrologueManager Instance
+    {
+        get
+        {
+            if (!_instance)
+            {
+                _instance = FindObjectOfType(typeof(PrologueManager)) as PrologueManager;
+                if (_instance == null)
+                    Debug.Log("no Singleton obj");
+            }
+            return _instance;
+        }
+    }
+
+    public void Start()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+        SoundManager.Instance.PlayBGM(BGM.Stage_Prologue);
+        panel_Index = 0;
+        Set_PageUI();
+
+    }
+
+    public void Set_PageUI()
+    {
+        if (panel_Index == 0)
+        {
+            BTN_prev.SetActive(false);
+        }
+        else
+        {
+            BTN_prev.SetActive(true);
+        }
+    }
+
+    public void Paging(bool isNext)
+    {
+        SoundManager.Instance.PlaySFX(SFX.click);
+
+        if (isNext)
+        {
+            if (panel_Index == Animation_Panels.Count-1)
+            {
+                GameManager.Instance.SetState(eState.Stage01);
+            }
+            else panel_Index ++;
+        }
+        else
+        {
+            panel_Index --;
+        }
+
+        Set_PageUI();
+
+        for (int i = 0; i < Animation_Panels.Count; i++)
+        {
+            if (i == panel_Index)
+            {
+                Animation_Panels[i].SetActive(true);
+            }
+            else
+            {
+                Animation_Panels[i].SetActive(false);
+            }
+        }
+    }
+
+    public void Click_Setting()
+    {
+        GameManager.Instance.Control_Setting();
+    }
+
+}
